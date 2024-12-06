d99e8b4064678cb62f0ff3fd3ec4b38db5762b54
SibelJr11 <107586362+SibelJr11@users.noreply.github.com>
2024-12-06 10:19:11 -0500

    Actualización y frontend

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <h1>HOLA</h1>
+    <h1>HOLA que tal</h1>
     <div className='drawer bg-green-50 w-full h-screen lg:drawer-open'>
        <input id="my-drawer-3" type='checkbox'  className='drawer-toggle'/>
        <div className='drawer-content relative'>
